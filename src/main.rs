df371f4e6b21d4b43a104fce54f54e0c5d5a39cf
alwafor <alwafor1@gmail.com>
2022-10-28 19:08:10 +0300

    refactor, split on some functions

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 use colored::Colorize;
 
 use std::{
-  env, fs,
+  env::{self, current_exe},
+  fs,
   io::Error,
   path::{Path, PathBuf},
 };
 
 fn main() {
+  let arguments = get_cli_arguments();
+  let file_text = get_file_text(&arguments[0]);
+  println!("{}", file_text.unwrap());
+}
+
+fn get_cli_arguments() -> Vec<String> {
   let args: Vec<String> = env::args().collect();
   args.get(1).unwrap_or_else(|| {
     panic!("Error! Arguments are not present!");
   });
-
-  let path_buf = Path::new(&args[0]).join(&"..").join(&args[1]);
-  let result = get_file_text(&path_buf);
-  println!("{}", result.unwrap());
+  args[1..].to_vec()
 }
 
-fn get_file_text(path: &PathBuf) -> Result<String, Error> {
-  let data = fs::read_to_string(path)?;
+fn get_file_text(path_part: &String) -> Result<String, Error> {
+  let path_buf = Path::new(&current_exe()?).join(&"..").join(path_part);
+
+  let data = fs::read_to_string(path_buf)?;
 
   Ok(data)
 }
